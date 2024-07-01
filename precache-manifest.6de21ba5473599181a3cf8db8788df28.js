d4d4347505e3c9501f8c594d0c97b95b00c44a37
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-07-01 12:20:48 +0530

    Updates

---
+++ precache-manifest.6de21ba5473599181a3cf8db8788df28.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "47e9f5cb2dbec72ac3a8",
+    "url": "/Whitewolf1237/PortfolioMain/static/css/main.13ee6a58.chunk.css"
+  },
+  {
+    "revision": "47e9f5cb2dbec72ac3a8",
+    "url": "/Whitewolf1237/PortfolioMain/static/js/main.c16dac5d.chunk.js"
+  },
+  {
+    "revision": "b10997c57e08af0ed343",
+    "url": "/Whitewolf1237/PortfolioMain/static/js/runtime~main.9c7a6711.js"
+  },
+  {
+    "revision": "7ab2499ecc4a0e625a99",
+    "url": "/Whitewolf1237/PortfolioMain/static/js/2.81dba7a2.chunk.js"
+  },
+  {
+    "revision": "4336ae61013b18ea587d7544b1e2aafe",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/react.4336ae61.svg"
+  },
+  {
+    "revision": "57dfc3496da6a66b92d54a1fd106b53c",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/firebase.57dfc349.svg"
+  },
+  {
+    "revision": "d9237725db7f9cfd32560a8a93505d4b",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/javascript.d9237725.svg"
+  },
+  {
+    "revision": "9571895b144e28775fd433e6e51b073e",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/python.9571895b.svg"
+  },
+  {
+    "revision": "22ee40702ee60e32869a6e80d97cf37b",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/flask.22ee4070.svg"
+  },
+  {
+    "revision": "1109c64899990f3bb31842aa87a41d9a",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/flutter.1109c648.svg"
+  },
+  {
+    "revision": "96986fcae124e6ff6f2b24ad4f999242",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/nodejs.96986fca.svg"
+  },
+  {
+    "revision": "682b59a561d6f9a9243b981e3395c95a",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/pytorch.682b59a5.png"
+  },
+  {
+    "revision": "7cce028487f05a5e5e778e013a0b4ce5",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/mysql.7cce0284.svg"
+  },
+  {
+    "revision": "1cafcd5c70970c6a5712e3262ccce0af",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/bootstrap.1cafcd5c.svg"
+  },
+  {
+    "revision": "b6dd61c9a165e83bdfd07e9fb07d3889",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/css.b6dd61c9.svg"
+  },
+  {
+    "revision": "521bdc40ebd94461995a83048f9dde23",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/html.521bdc40.svg"
+  },
+  {
+    "revision": "c7c8e06dfba50ecc8df0ac14121565ac",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/tailwind.c7c8e06d.svg"
+  },
+  {
+    "revision": "8bb84bc75cc3987b545cda66570a5783",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/redux.8bb84bc7.svg"
+  },
+  {
+    "revision": "49e2724a4ad3ae481747a67eabfdd167",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/crunch.49e2724a.png"
+  },
+  {
+    "revision": "c84292125d9d7d6c0f82004eda60698e",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/dyce&dyne.c8429212.png"
+  },
+  {
+    "revision": "527dedb80b395a6d67547d5d8761c437",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/earthling.527dedb8.png"
+  },
+  {
+    "revision": "894cd311626aa1b40d9b94295b794ac2",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/khetibadi.894cd311.png"
+  },
+  {
+    "revision": "cc27e5c926ffe7d15a5cb1428990b184",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/foco.cc27e5c9.png"
+  },
+  {
+    "revision": "c668d9a03acda77b0e4d64d8a403b493",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/chaptersqueue.c668d9a0.png"
+  },
+  {
+    "revision": "3d2e9ff1547d4739c38dd087fc1002c8",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/mrc.3d2e9ff1.png"
+  },
+  {
+    "revision": "aa79e31c29e4782f0d5cd2eaee216ef5",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/fyp.aa79e31c.png"
+  },
+  {
+    "revision": "0e77c087bb51d662f9feb1923be9a72f",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/bufferoverflow.0e77c087.png"
+  },
+  {
+    "revision": "17ab1b8cc653f65d181ffbc646a13be8",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/NetflixClone.17ab1b8c.png"
+  },
+  {
+    "revision": "931929f5c5e7dc758a5cbd01682b813c",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/SwiggyClone.931929f5.png"
+  },
+  {
+    "revision": "925d816d618e4001a26eacb9266343e1",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/aboutme2.925d816d.png"
+  },
+  {
+    "revision": "c95c3642f8714cd2c01996eb17690632",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/c++.c95c3642.svg"
+  },
+  {
+    "revision": "2bc1093d824833bdcf86f8cad530246a",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/java.2bc1093d.svg"
+  },
+  {
+    "revision": "6a3bfe5d1247b2d620debf98f4b6dc7a",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/mongodb.6a3bfe5d.svg"
+  },
+  {
+    "revision": "ab2bf628866bdff2d34d6d31e4ea0667",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/android.ab2bf628.svg"
+  },
+  {
+    "revision": "58c36dd6ab1d6f1761d06c023f570417",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/jquery.58c36dd6.svg"
+  },
+  {
+    "revision": "d51cfa4a35e802b60c57f166cdf288fd",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/git.d51cfa4a.svg"
+  },
+  {
+    "revision": "69b034d02c14ae459747c706e2dd47a0",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/json.69b034d0.svg"
+  },
+  {
+    "revision": "ba25f54326ded54c8aea9c5abbda36c4",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/android-studio.ba25f543.svg"
+  },
+  {
+    "revision": "c26a9e54e264ea0879b71ad6975b78fd",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/github.c26a9e54.svg"
+  },
+  {
+    "revision": "a8f96c5fe186814585782009101d4c94",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/intellij.a8f96c5f.svg"
+  },
+  {
+    "revision": "0ec516f71773d73edcdf57252016c869",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/vscode.0ec516f7.svg"
+  },
+  {
+    "revision": "8c6893f5f98107b933d9bd6db89bd615",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/cupy.8c6893f5.png"
+  },
+  {
+    "revision": "bcf7b3c74d00d6ed1601f4bc7bcc015d",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/numpy.bcf7b3c7.png"
+  },
+  {
+    "revision": "7bf8d5d293f52d7d627e4d8175ec952f",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/pandas.7bf8d5d2.png"
+  },
+  {
+    "revision": "f0f50003d0e332fe8232c3d685b6b221",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/sklearn.f0f50003.png"
+  },
+  {
+    "revision": "60588e5b05564542dd2b85bdae288413",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/tmdb.60588e5b.svg"
+  },
+  {
+    "revision": "6d25ae389f5f8ecf7f9dcb80076de116",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/c.6d25ae38.svg"
+  },
+  {
+    "revision": "9da33b058bf8980a407c2cd6840d0b91",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/dart.9da33b05.svg"
+  },
+  {
+    "revision": "189aacfefda1c78e7825dad5528d5abe",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/typescript.189aacfe.svg"
+  },
+  {
+    "revision": "b4cebee8c22bf92d081f87e5272153bf",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/julia.b4cebee8.png"
+  },
+  {
+    "revision": "6c767968df45e56a950223f08199bdeb",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/postgresql.6c767968.svg"
+  },
+  {
+    "revision": "449e8efbf3e642eecc4d8e0b1fe7cb1d",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/spring.449e8efb.svg"
+  },
+  {
+    "revision": "4d52d08b29f1f9031f23ac847c18f974",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/flux.4d52d08b.png"
+  },
+  {
+    "revision": "29266fc28defdad9c6120568eb2a871f",
+    "url": "/Whitewolf1237/PortfolioMain/static/media/LangChain.29266fc2.png"
+  },
+  {
+    "revision": "b1f6ee6742757e141f61e0c65fa23f5f",
+    "url": "/Whitewolf1237/PortfolioMain/index.html"
+  }
+];
